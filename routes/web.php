027b00449a97cf5a7df01e62f940e55cfaa02f5b
adansyah <adansyah225@gmail.com>
2025-06-08 16:21:05 +0700

    four commit

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/bos', function () {
-    return view('admin.dashboard');
-});
+Route::resource('admins', AdminController::class);
