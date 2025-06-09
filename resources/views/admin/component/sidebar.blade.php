966d31a30bb42ea4c10743d006deeef949b90ba6
adansyah <adansyah225@gmail.com>
2025-06-09 14:20:42 +0700

    first commit

--- resources/views/admin/component/sidebar.blade.php
+++ resources/views/admin/component/sidebar.blade.php
@@ -11,17 +11,20 @@
                 <li class="nav-small-cap"><span class="hide-menu">Applications</span></li>
 
                 <li class="sidebar-item"> <a class="sidebar-link" href="ticket-list.html" aria-expanded="false"><i
-                            data-feather="tag" class="feather-icon"></i><span class="hide-menu">Ticket List
+                            data-feather="archive" class="feather-icon"></i><span class="hide-menu">Kategori
                         </span></a>
                 </li>
                 <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-chat.html"
-                        aria-expanded="false"><i data-feather="message-square" class="feather-icon"></i><span
-                            class="hide-menu">Chat</span></a></li>
+                        aria-expanded="false"><i data-feather="coffee" class="feather-icon"></i><span
+                            class="hide-menu">Menu</span></a></li>
                 <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-calendar.html"
-                        aria-expanded="false"><i data-feather="calendar" class="feather-icon"></i><span
-                            class="hide-menu">Calendar</span></a></li>
+                        aria-expanded="false"><i data-feather="box" class="feather-icon"></i><span
+                            class="hide-menu">Stok</span></a></li>
+                <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-calendar.html"
+                        aria-expanded="false"><i data-feather="user" class="feather-icon"></i><span
+                            class="hide-menu">Pengguna</span></a></li>
 
-                <li class="list-divider"></li>
+                {{-- <li class="list-divider"></li>
                 <li class="nav-small-cap"><span class="hide-menu">Components</span></li>
                 <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)"
                         aria-expanded="false"><i data-feather="file-text" class="feather-icon"></i><span
@@ -143,76 +146,35 @@
                         aria-expanded="false"><i data-feather="sidebar" class="feather-icon"></i><span
                             class="hide-menu">Cards
                         </span></a>
-                </li>
+                </li> --}}
                 <li class="list-divider"></li>
-                <li class="nav-small-cap"><span class="hide-menu">Authentication</span></li>
+                <li class="nav-small-cap"><span class="hide-menu">Processing</span></li>
 
                 <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="authentication-login1.html"
-                        aria-expanded="false"><i data-feather="lock" class="feather-icon"></i><span
-                            class="hide-menu">Login
+                        aria-expanded="false"><i data-feather="shopping-bag" class="feather-icon"></i><span
+                            class="hide-menu">Laporan Penjualan
                         </span></a>
                 </li>
                 <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="authentication-register1.html"
-                        aria-expanded="false"><i data-feather="lock" class="feather-icon"></i><span
-                            class="hide-menu">Register
+                        aria-expanded="false"><i data-feather="file-text" class="feather-icon"></i><span
+                            class="hide-menu">Ulasan Pelanggan
+                        </span></a>
+                </li>
+                <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="authentication-register1.html"
+                        aria-expanded="false"><i data-feather="credit-card" class="feather-icon"></i><span
+                            class="hide-menu">Riwayat Transaksi
                         </span></a>
                 </li>
 
-                <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)"
-                        aria-expanded="false"><i data-feather="feather" class="feather-icon"></i><span
-                            class="hide-menu">Icons
-                        </span></a>
-                    <ul aria-expanded="false" class="collapse first-level base-level-line">
-                        <li class="sidebar-item"><a href="icon-fontawesome.html" class="sidebar-link"><span
-                                    class="hide-menu"> Fontawesome Icons </span></a></li>
 
-                        <li class="sidebar-item"><a href="icon-simple-lineicon.html" class="sidebar-link"><span
-                                    class="hide-menu"> Simple Line Icons </span></a>
-                        </li>
-                    </ul>
-                </li>
 
 
                 <li class="list-divider"></li>
-                <li class="nav-small-cap"><span class="hide-menu">Extra</span></li>
+                <li class="nav-small-cap"><span class="hide-menu">Setting</span></li>
 
                 <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="authentication-login1.html"
                         aria-expanded="false"><i data-feather="log-out" class="feather-icon"></i><span
                             class="hide-menu">Logout</span></a></li>
-                <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)"
-                        aria-expanded="false"><i data-feather="crosshair" class="feather-icon"></i><span
-                            class="hide-menu">Multi
-                            level
-                            dd</span></a>
-                    <ul aria-expanded="false" class="collapse first-level base-level-line">
-                        <li class="sidebar-item"><a href="javascript:void(0)" class="sidebar-link"><span
-                                    class="hide-menu"> item 1.1</span></a>
-                        </li>
-                        <li class="sidebar-item"><a href="javascript:void(0)" class="sidebar-link"><span
-                                    class="hide-menu"> item 1.2</span></a>
-                        </li>
-                        <li class="sidebar-item"> <a class="has-arrow sidebar-link" href="javascript:void(0)"
-                                aria-expanded="false"><span class="hide-menu">Menu 1.3</span></a>
-                            <ul aria-expanded="false" class="collapse second-level base-level-line">
-                                <li class="sidebar-item"><a href="javascript:void(0)" class="sidebar-link"><span
-                                            class="hide-menu"> item
-                                            1.3.1</span></a></li>
-                                <li class="sidebar-item"><a href="javascript:void(0)" class="sidebar-link"><span
-                                            class="hide-menu"> item
-                                            1.3.2</span></a></li>
-                                <li class="sidebar-item"><a href="javascript:void(0)" class="sidebar-link"><span
-                                            class="hide-menu"> item
-                                            1.3.3</span></a></li>
-                                <li class="sidebar-item"><a href="javascript:void(0)" class="sidebar-link"><span
-                                            class="hide-menu"> item
-                                            1.3.4</span></a></li>
-                            </ul>
-                        </li>
-                        <li class="sidebar-item"><a href="javascript:void(0)" class="sidebar-link"><span
-                                    class="hide-menu"> item
-                                    1.4</span></a></li>
-                    </ul>
-                </li>
             </ul>
         </nav>
         <!-- End Sidebar navigation -->
