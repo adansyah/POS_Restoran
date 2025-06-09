75cb63597f2f268f12b97c2d40c3082e0ae1dd31
adansyah <adansyah225@gmail.com>
2025-06-09 14:45:29 +0700

    first commit

---
+++ resources/views/dapur/component/sidebar.blade.php
@@ -0,0 +1,32 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="dropdown active">
+                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+
+            </li>
+            <li class="menu-header">Pesanan</li>
+
+            <li><a class="nav-link" href="blank.html"><i class="far fa-circle"></i> <span>Pesanan Masuk</span></a>
+            </li>
+            <li><a class="nav-link" href="blank.html"><i class="far fa-times-circle"></i> <span>Pesanan
+                        Diproses</span></a>
+            </li>
+            <li><a class="nav-link" href="blank.html"><i class="far fa-check-circle"></i> <span>Pesanan Siap</span></a>
+            </li>
+        </ul>
+
+        <div class="mb-4 p-3 hide-sidebar-mini">
+            <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+                <i class="fas fa-sign-out-alt"></i> Logout
+            </a>
+        </div>
+    </aside>
+</div>
