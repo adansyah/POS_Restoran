ee0bce0023d39a06e146e307d6f87bfbc92c2a63
adansyah <adansyah225@gmail.com>
2025-06-09 14:32:15 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DapurController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('admins', AdminController::class);
+Route::get('kitchen', [DapurController::class, 'index']);
