c1753c6b150fec70ae39340ee10850e00c449e8b
adansyah <adansyah225@gmail.com>
2025-06-11 14:21:42 +0700

    update admin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DapurController;
+use App\Http\Controllers\MenuController;
 use App\Livewire\Admin\Dashboard;
 use Illuminate\Support\Facades\Route;
 use Livewire\Livewire;
@@ -15,6 +16,7 @@ Route::get('/', function () {
 
 Route::resource('admins', AdminController::class);
 Route::resource('kategori', CategoryController::class);
+Route::resource('menu', MenuController::class);
 Route::middleware(['auth', 'cekrole:admin'])->group(function () {});
 
 Route::middleware(['auth', 'cekrole:dapur'])->group(function () {
